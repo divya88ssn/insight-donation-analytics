8a6a1a8260ef692d2ee0cef3f5e0d198549fd611
Divya Guna <divya88ssn@gmail.com>
2018-02-08 22:04:19 -0500

    pushing changes to chmod for ip, op and run.sh scripts

---
+++ src/master_scheduler.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+import sys
+
+dataFile = sys.argv[1]
+percentFile = sys.argv[2]
+print dataFile
+print percentFile
+#file = open("../input/itcont.txt","r")
+#print file.readline(1)
+#file.close()
