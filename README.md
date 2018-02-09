61396d8d7f0d7f37dcc679ff63b758cbe62ab89c
Divya Guna <divya88ssn@gmail.com>
2018-02-09 18:36:11 -0500

    updating latest local changes

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # insight-donation-analytics
 A project to analyse repeat election campaign contributions
+
+Software tools and packages:
+----------------------------
+This project was built using bash and python scripts
+and tested on an ubuntu linux desktop distro.
+
+The versions are listed as follows:
+
+python -V
+python 2.7.13
+
+pip install sortedcontainers
+Collecting sortedcontainers
+  Downloading sortedcontainers-1.5.9-py2.py3-none-any.whl
+Installing collected packages: sortedcontainers
+Successfully installed sortedcontainers-1.5.9
+
+bash --version
+GNU bash, version 4.4.7(1)-release (x86_64-pc-linux-gnu)
+Copyright (C) 2016 Free Software Foundation, Inc.
+License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>
--- src/donation_analytics.py
+++ src/donation_analytics.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python
 import sys
 import os
+from sortedcontainers import SortedList
 
 def main(argv) :
 	#validate input args
 	index_len = len(sys.argv)
 	if (index_len < 4) :
-		print "donation_analytics.txt needs temp ip file, percentile value and op file name\n"
+		print "donation_analytics.txt needs ip file, percentile and op file values\n"
 		sys.exit(1)
 	else :
 		processFile = argv[1]
@@ -15,7 +16,9 @@ def main(argv) :
 
 	donorNameZip = {} #dictionary which uses donor name+zip as key
 	totalAmount = {} #dictionary which uses recipient+zip+year as key
-	totalTransactions = {} #dictionary that uses recipient+zip+year as key
+	totalTransactions = {} #dictionary which uses recipient+zip+year as key
+	zipYrDict = {} #dictionary that uses zip+year as keys and\
+			#SortedList() to store repeat contribution amount for a given zip+year
 
 	#create and write to the designated output file
 	opHandle = open(opFile, "w+")
@@ -44,6 +47,17 @@ def main(argv) :
 			else :
 				sum = totalTransactions[recipientId] + 1
 				totalTransactions[recipientId] = sum
+
+			#for percentile calculation
+			zipYrKey = fields[2]+fields[3][-4:]
+			if not zipYrDict.has_key(zipYrKey) :
+				amtList = SortedList()
+				amtList.add(long(fields[4]))
+				zipYrDict.update({zipYrKey:amtList})
+			else :
+				myList = zipYrDict[zipYrKey]
+				myList.add(long(fields[4]))
+				zipYrDict.update({zipYrKey:myList})
 
 			opHandle.write(fields[0]+'|'+fields[2]+'|'+
 					fields[3][-4:]+'|'+""+'|'+str(totalAmount[recipientId])+'|'+
