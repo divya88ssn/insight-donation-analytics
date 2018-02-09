0336513931def9b999d9a513c2eb1ff0692bc986
Divya Guna <divya88ssn@gmail.com>
2018-02-09 10:03:53 -0500

    run.sh passes proper path variable to src script. master_scheduler.py outputs valid lines from itcont.txt

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 #pass input and output file location to the source code
-#python ./master_scheduler.py $1 $2 $3
-python src/master_scheduler.py "../input/itcont.txt" "../input/percentile.txt" "../output/repeat_donors.txt"
+python src/master_scheduler.py input/itcont.txt input/percentile.txt output/repeat_donors.txt
--- src/master_scheduler.py
+++ src/master_scheduler.py
@@ -1,11 +1,49 @@
 #!/usr/bin/python
-
 import sys
+import os
 
-dataFile = sys.argv[1]
-percentFile = sys.argv[2]
-print dataFile
-print percentFile
-#file = open("../input/itcont.txt","r")
-#print file.readline(1)
-#file.close()
+def main(argv):
+	#validate input args
+	index_len = len(sys.argv)
+	if (index_len < 4):
+		print "No input args, Need ip files and op location info\n"
+		sys.exit(1)
+	else:
+		dataFile = argv[1]
+		percentFile = argv[2]
+		opFile = argv[3]
+
+	if (dataFile == "" or percentFile == ""):
+		print "Empty input file info\n"
+		sys.exit(1)
+	elif (opFile == ""):
+		print "op file information missing\n"
+		sys.exit(1)
+
+	if (index_len > 4):
+		print "Unwanted additional input info warning, execution contd...\n"
+
+	#open input file and read contents
+	if not os.path.exists(dataFile):
+		print "Error," + " " + dataFile + "  doesnt exist\n"
+		sys.exit(1)
+
+	ipFile = open(dataFile,"r")
+	for line in ipFile:
+		fields = line.split('|')
+		if (
+			fields[15] == "" and fields[0] != "" and
+			fields[7] != "" and fields[10] != "" and
+			fields[13] != "" and fields[14] != ""
+		   ):
+			print fields[0]	#recipient id
+			print fields[7] #name of donor
+			print fields[10][:5] #zipcode of donor
+			print fields[13] #date of donation
+			print fields[14] #donation amount
+			print "\n"
+	ipFile.close()
+	sys.exit(0)
+
+if __name__ == "__main__":
+	main(sys.argv)
